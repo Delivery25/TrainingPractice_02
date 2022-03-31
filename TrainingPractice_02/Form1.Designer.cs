990e1ede811f436865e5e3b82e09401880546957
Di <dds.03@mail.ru>
2022-03-31 14:07:52 +0300

    Добавьте файлы проекта.

---
+++ TrainingPractice_02/Form1.Designer.cs
@@ -0,0 +1,228 @@
+﻿
+namespace TrainingPractice_02
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.файлToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openFirstFileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openSecondFileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.compareFilesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.settingsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.colorSelectAddedWordToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.colorSelectEditedWordToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.colorSelectRemovedWordToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.colorFontToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.styleFontToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.файлToolStripMenuItem,
+            this.compareFilesToolStripMenuItem,
+            this.settingsToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(652, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // файлToolStripMenuItem
+            // 
+            this.файлToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openFirstFileToolStripMenuItem,
+            this.openSecondFileToolStripMenuItem});
+            this.файлToolStripMenuItem.Name = "файлToolStripMenuItem";
+            this.файлToolStripMenuItem.Size = new System.Drawing.Size(48, 20);
+            this.файлToolStripMenuItem.Text = "Файл";
+            // 
+            // openFirstFileToolStripMenuItem
+            // 
+            this.openFirstFileToolStripMenuItem.Name = "openFirstFileToolStripMenuItem";
+            this.openFirstFileToolStripMenuItem.Size = new System.Drawing.Size(226, 22);
+            this.openFirstFileToolStripMenuItem.Text = "Открыть исходный файл";
+            this.openFirstFileToolStripMenuItem.Click += new System.EventHandler(this.openFirstFileToolStripMenuItem_Click);
+            // 
+            // openSecondFileToolStripMenuItem
+            // 
+            this.openSecondFileToolStripMenuItem.Name = "openSecondFileToolStripMenuItem";
+            this.openSecondFileToolStripMenuItem.Size = new System.Drawing.Size(226, 22);
+            this.openSecondFileToolStripMenuItem.Text = "Открыть изменённый файл";
+            this.openSecondFileToolStripMenuItem.Click += new System.EventHandler(this.openSecondFileToolStripMenuItem_Click);
+            // 
+            // compareFilesToolStripMenuItem
+            // 
+            this.compareFilesToolStripMenuItem.Enabled = false;
+            this.compareFilesToolStripMenuItem.Name = "compareFilesToolStripMenuItem";
+            this.compareFilesToolStripMenuItem.Size = new System.Drawing.Size(112, 20);
+            this.compareFilesToolStripMenuItem.Text = "Сравнить файлы";
+            this.compareFilesToolStripMenuItem.Click += new System.EventHandler(this.compareFilesToolStripMenuItem_Click);
+            // 
+            // settingsToolStripMenuItem
+            // 
+            this.settingsToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.colorSelectAddedWordToolStripMenuItem,
+            this.colorSelectEditedWordToolStripMenuItem,
+            this.colorSelectRemovedWordToolStripMenuItem,
+            this.colorFontToolStripMenuItem,
+            this.styleFontToolStripMenuItem});
+            this.settingsToolStripMenuItem.Name = "settingsToolStripMenuItem";
+            this.settingsToolStripMenuItem.Size = new System.Drawing.Size(79, 20);
+            this.settingsToolStripMenuItem.Text = "Настройки";
+            // 
+            // colorSelectAddedWordToolStripMenuItem
+            // 
+            this.colorSelectAddedWordToolStripMenuItem.Name = "colorSelectAddedWordToolStripMenuItem";
+            this.colorSelectAddedWordToolStripMenuItem.Size = new System.Drawing.Size(269, 22);
+            this.colorSelectAddedWordToolStripMenuItem.Text = "Цвет выделения добавленных слов";
+            this.colorSelectAddedWordToolStripMenuItem.Click += new System.EventHandler(this.colorSelectAddedWordToolStripMenuItem_Click);
+            // 
+            // colorSelectEditedWordToolStripMenuItem
+            // 
+            this.colorSelectEditedWordToolStripMenuItem.Name = "colorSelectEditedWordToolStripMenuItem";
+            this.colorSelectEditedWordToolStripMenuItem.Size = new System.Drawing.Size(269, 22);
+            this.colorSelectEditedWordToolStripMenuItem.Text = "Цвет выделения изменённых слов";
+            this.colorSelectEditedWordToolStripMenuItem.Click += new System.EventHandler(this.colorSelectEditedWordToolStripMenuItem_Click);
+            // 
+            // colorSelectRemovedWordToolStripMenuItem
+            // 
+            this.colorSelectRemovedWordToolStripMenuItem.Name = "colorSelectRemovedWordToolStripMenuItem";
+            this.colorSelectRemovedWordToolStripMenuItem.Size = new System.Drawing.Size(269, 22);
+            this.colorSelectRemovedWordToolStripMenuItem.Text = "Цвет выделения удалённых слов";
+            this.colorSelectRemovedWordToolStripMenuItem.Click += new System.EventHandler(this.colorSelectRemovedWordToolStripMenuItem_Click);
+            // 
+            // colorFontToolStripMenuItem
+            // 
+            this.colorFontToolStripMenuItem.Name = "colorFontToolStripMenuItem";
+            this.colorFontToolStripMenuItem.Size = new System.Drawing.Size(269, 22);
+            this.colorFontToolStripMenuItem.Text = "Цвет шрифта";
+            this.colorFontToolStripMenuItem.Click += new System.EventHandler(this.colorFontToolStripMenuItem_Click);
+            // 
+            // styleFontToolStripMenuItem
+            // 
+            this.styleFontToolStripMenuItem.Name = "styleFontToolStripMenuItem";
+            this.styleFontToolStripMenuItem.Size = new System.Drawing.Size(269, 22);
+            this.styleFontToolStripMenuItem.Text = "Стиль шрифта";
+            this.styleFontToolStripMenuItem.Click += new System.EventHandler(this.styleFontToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(54, 20);
+            this.exitToolStripMenuItem.Text = "Выход";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Cursor = System.Windows.Forms.Cursors.VSplit;
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 24);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.richTextBox1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.richTextBox2);
+            this.splitContainer1.Size = new System.Drawing.Size(652, 420);
+            this.splitContainer1.SplitterDistance = 325;
+            this.splitContainer1.TabIndex = 1;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.richTextBox1.Location = new System.Drawing.Point(0, 0);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(325, 420);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.richTextBox2.Location = new System.Drawing.Point(0, 0);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(323, 420);
+            this.richTextBox2.TabIndex = 0;
+            this.richTextBox2.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(652, 444);
+            this.Controls.Add(this.splitContainer1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Программа для визуального сравнения текстовых файлов";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem файлToolStripMenuItem;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.ToolStripMenuItem openFirstFileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openSecondFileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem compareFilesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem settingsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem colorSelectAddedWordToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem colorSelectEditedWordToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem colorSelectRemovedWordToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem colorFontToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem styleFontToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+    }
+}
+
---
+++ TrainingPractice_02/Form1.cs
@@ -0,0 +1,322 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Windows.Forms;
+
+namespace TrainingPractice_02
+{
+    public partial class Form1 : Form
+    {
+        static Font font = new Font("Times New Roman", 14);
+        static bool pressed1 = false, pressed2 = false;
+        ColorDialog colorDialog1 = new ColorDialog();
+        static string[][] oldText, newText;
+        static int[] oldEndOfSentences, newEndOfSentences;
+        static int[] oldStartOfSentences, newStartOfSentences;
+        static Color addColor = Color.LightGreen;
+        static Color removeColor = Color.Red;
+        static Color editColor = Color.Yellow;
+
+        static List<Selection> selections = new List<Selection>();
+
+        struct Selection
+        {
+            public int Start;
+            public int End;
+            public Color Color;
+            public RichTextBox RichTextBox;
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+            richTextBox1.Font = font;
+            richTextBox2.Font = font;
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            DialogResult res = MessageBox.Show("Завершить работу?", "Предупреждение", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+            if (res == DialogResult.Yes)
+                Application.Exit();
+        }
+
+        private void colorSelectAddedWordToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (colorDialog1.ShowDialog() == DialogResult.OK)
+            {
+                addColor = colorDialog1.Color;
+                compareFilesToolStripMenuItem.PerformClick();
+            }
+        }
+
+        private void colorSelectEditedWordToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (colorDialog1.ShowDialog() == DialogResult.OK)
+            {
+                editColor = colorDialog1.Color;
+                compareFilesToolStripMenuItem.PerformClick();
+            }
+        }
+
+        private void colorSelectRemovedWordToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (colorDialog1.ShowDialog() == DialogResult.OK)
+            {
+                removeColor = colorDialog1.Color;
+                compareFilesToolStripMenuItem.PerformClick();
+            }
+        }
+
+        private void colorFontToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (colorDialog1.ShowDialog() == DialogResult.OK)
+            {
+                richTextBox1.ForeColor = colorDialog1.Color;
+                richTextBox2.ForeColor = colorDialog1.Color;
+            }
+        }
+
+        private void styleFontToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            var fontDialog1 = new FontDialog();
+            if (fontDialog1.ShowDialog() == DialogResult.OK)
+            {
+                richTextBox2.Font = fontDialog1.Font;
+                richTextBox1.Font = fontDialog1.Font;
+            }
+        }
+        private void openFirstFileToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (normalizeInput(ref oldText, ref oldEndOfSentences, ref oldStartOfSentences))
+            {
+                ClearSelection();
+                fillRichTextBox(richTextBox1, oldText);
+            }
+            if (richTextBox1.TextLength != 0)
+            {
+                pressed1 = true;
+                if (pressed1 && pressed2)
+                {
+                    compareFilesToolStripMenuItem.Enabled = true;
+                }
+            }
+        }
+
+        private void openSecondFileToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (normalizeInput(ref newText, ref newEndOfSentences, ref newStartOfSentences))
+            {
+                ClearSelection();
+                fillRichTextBox(richTextBox2, newText);
+            }
+            if (richTextBox2.TextLength != 0)
+            {
+                pressed2 = true;
+                if (pressed1 && pressed2)
+                {
+                    compareFilesToolStripMenuItem.Enabled = true;
+                }
+            }
+        }
+
+        private bool normalizeInput(ref string[][] text, ref int[] endOfSentences, ref int[] startOfSentences)
+        {
+            var result = false;
+            var file = string.Empty;
+            var openFileDialog = new OpenFileDialog
+            {
+                Filter = "Text Files | *.txt",
+                FileName = string.Empty
+            };
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                result = true;
+                var indexes = new List<int>();
+                file = File.ReadAllText(openFileDialog.FileName);
+                for (int i = 0; i < file.Length; i++)
+                {
+                    if (file[i] == '.' ||
+                        file[i] == '?' ||
+                        file[i] == '!')
+                    {
+                        indexes.Add(i);
+                    }
+                }
+                text = new string[indexes.Count][];
+                var start = 0;
+                for (int i = 0; i < indexes.Count; i++)
+                {
+                    var sentence = file[start..(indexes[i] + 1)];
+                    text[i] = sentence.Split(' ');
+                    start = indexes[i] + 2;
+                }
+                endOfSentences = indexes.ToArray();
+
+                startOfSentences = new int[endOfSentences.Length];
+                startOfSentences[0] = 0;
+
+                for (int i = 0; i < endOfSentences.Length - 1; i++)
+                {
+                    startOfSentences[i + 1] = endOfSentences[i] + 2;
+                }
+            }
+            return result;
+        }
+
+        private void fillRichTextBox(RichTextBox richTextBox, string[][] text)
+        {
+            richTextBox.Clear();
+            foreach (var item in text)
+            {
+                foreach (var item2 in item)
+                {
+                    richTextBox.Text += item2 + ' ';
+                }
+            }
+
+            foreach (var item in selections)
+            {
+                item.RichTextBox.Select(item.Start, item.End);
+                item.RichTextBox.SelectionBackColor = item.Color;
+            }
+        }
+
+        private void ClearSelection()
+        {
+            selections.Clear();
+            richTextBox1.SelectAll();
+            richTextBox1.SelectionBackColor = Color.White;
+            richTextBox2.SelectAll();
+            richTextBox2.SelectionBackColor = Color.White;
+            richTextBox1.DeselectAll();
+            richTextBox2.DeselectAll();
+        }
+
+        private void compareFilesToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            selections.Clear();
+            if (richTextBox1.Text != richTextBox2.Text)
+            {
+                for (int i = 0; i < oldText.GetLength(0); i++)
+                {
+                    CompareSentences(oldText[i], newText[i], i);
+                }
+
+                fillRichTextBox(richTextBox1, oldText);
+                fillRichTextBox(richTextBox2, newText);
+                richTextBox1.Select(0, 0);
+                richTextBox2.Select(0, 0);
+            }
+            else
+            {
+                MessageBox.Show("Файлы идентичны", "Сообщение", MessageBoxButtons.OK);
+            }
+        }
+
+        private bool CompareSentences(string[] oldSentence, string[] newSentence, int number)
+        {
+            var sentenceSelection = new List<Selection>();
+            var isWordCountEqual = false;
+
+            if (oldSentence.Length == newSentence.Length)
+            {
+                isWordCountEqual = true;
+            }
+
+            if (isWordCountEqual)
+            {
+                //поиск всех модификаций в словах и выделение жёлтым
+                var sum = 0;
+                for (int i = 0; i < oldSentence.Length; i++)
+                {
+
+                    if (oldSentence[i] != newSentence[i])
+                    {
+                        var selection = new Selection();
+                        selection.Start = newStartOfSentences[number] + sum;
+                        selection.End = newSentence[i].Length;
+                        selection.Color = editColor;
+                        selection.RichTextBox = richTextBox2;
+                        sentenceSelection.Add(selection);
+                    }
+                    sum += newSentence[i].Length + 1;
+                }
+                selections.AddRange(sentenceSelection);
+                return true;
+            }
+            else
+            {
+                var offset = 0;
+                //проверка на изменение слов в общем
+                for (int i = 0; i < oldSentence.Length; i++)
+                {
+                    if (oldSentence[i] != newSentence[i + offset])
+                    {
+                        var selection = new Selection();
+                        var start = 0;
+                        var temp = offset;
+                        while (temp != newSentence.Length - i &&
+                               oldSentence[i] != newSentence[i + temp])
+                        {
+                            temp++;
+                        }
+                        if (temp == newSentence.Length - i)
+                        {
+                            temp = 0;
+                            while (temp != oldSentence.Length - i &&
+                               newSentence[i + offset] != oldSentence[i + temp])
+                            {
+                                temp++;
+                            }
+                            if (temp == oldSentence.Length - i)
+                            {
+                                return false;
+                            }
+                            else
+                            {
+                                for (int j = 0; j < i; j++)
+                                {
+                                    start += oldSentence[j].Length + 1;
+                                }
+                                var end = 0;
+                                for (int j = i; j < i + temp; j++)
+                                {
+                                    end += oldSentence[j].Length + 1;
+                                }
+                                selection.Start = start + oldStartOfSentences[number];
+                                selection.End = end - 1;
+                                selection.Color = removeColor;
+                                selection.RichTextBox = richTextBox1;
+                                sentenceSelection.Add(selection);
+                                offset--;
+                            }
+                        }
+                        else
+                        {
+                            for (int j = 0; j < i + offset; j++)
+                            {
+                                start += newSentence[j].Length + 1;
+                            }
+                            var end = 0;
+                            for (int j = i + offset; j < i + temp; j++)
+                            {
+                                end += newSentence[j].Length + 1;
+                            }
+                            selection.Start = start + newStartOfSentences[number];
+                            selection.End = end - 1;
+                            selection.Color = addColor;
+                            selection.RichTextBox = richTextBox2;
+                            sentenceSelection.Add(selection);
+                            offset = temp;
+                        }
+                    }
+
+                }
+                selections.AddRange(sentenceSelection);
+                return true;
+            }
+        }
+    }
+}
